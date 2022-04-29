f762f137294a02b91a5500e86c77628106911ead
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-04-29 14:08:55 -0400

    added functionality to mainActivity / getting dummy from viewmodel

--- app/src/main/java/com/android/countrieslist/view/MainActivity.kt
+++ app/src/main/java/com/android/countrieslist/view/MainActivity.kt
@@ -2,11 +2,51 @@ package com.android.countrieslist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.android.countrieslist.R
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.countrieslist.databinding.ActivityMainBinding
+import com.android.countrieslist.viewmodel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+
+        binding.countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+        observeViewModel()
+    }
+
+    fun observeViewModel(){
+        viewModel.countries.observe(this, Observer {countries ->
+            countries.let{countriesAdapter.updateCountries(it)}
+        })
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let{binding.listError.visibility = if(it) View.VISIBLE else View.GONE}
+        })
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let { binding.loadingView.visibility = if(it) View.VISIBLE else View.GONE
+            if(it){
+                binding.listError.visibility = View.GONE
+                binding.countriesList.visibility = View.GONE
+            }
+            }
+        })
     }
 }
